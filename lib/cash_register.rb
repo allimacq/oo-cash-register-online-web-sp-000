f24b66623f18f0a133d331c819344d02614de473
Allison MacQueen <allimacq@gmail.com>
2020-09-29 19:37:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -37,16 +37,19 @@ class CashRegister
     end
   end
   
-  def items
-    #how many times to repeat the item in the array given the quantity
-    item_quantity = "#{title} " * @quantity
-    #converting to an array
-    items = item_quantity.split
-    ITEMS.push(*items)
-    p ITEMS
+  #def items
+    ##how many times to repeat the item in the array given the quantity
+    #item_quantity = "#{title} " * @quantity
+    ##converting to an array
+    #items = item_quantity.split
+    #ITEMS.push(*items)
+    #p ITEMS
    ##HOW DO I ADD THE EGGS?!
     #p ITEMS
-  end 
+  #end 
+  
+  def items
+  end
   
   
 end
